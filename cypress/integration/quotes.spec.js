4bf1b4e7a55f68a3996a901a7904ae40aaa1658a
Casey Harding <casey.r.harding@gmail.com>
2022-05-26 12:38:11 -0400

    Initial getters in place.

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,17 @@
 // write tests here
+describe("Quotes App", () => {
+  // Each test needs "fresh" state!!
+  // Tests should never rely on state left over from other tests
+  // Every test must be able to work in isolation
+  beforeEach(() => {
+    cy.visit("http://localhost:1234");
+  })
+
+  // Helpers to consolidate our getters...
+
+  const textInput = () => cy.get("input[name=text]");
+  const authorInput = () => cy.get("input[name=author]");
+  const foobarInput = () => cy.get("input[name=foobar]");
+  const submitBtn = () => cy.get(`button[id="submitBtn"]`);
+  const cancelBtn = () => cy.get(`button[id="cancelBtn"]`);
+})
